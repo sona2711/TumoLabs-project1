4b2215c2bb040a36f341f53be9f2d1e42d262394
sona2711 <sonamkrtchyan2711@gmail.com>
2023-09-26 00:21:07 +0400

    new version

---
+++ swap.js
@@ -0,0 +1,24 @@
+'use strict';
+
+document.querySelector('#clickme').addEventListener('click', function() {
+    const myText = document.querySelector('input').value;
+    const myChar = myText.match(/[a-zA-Z]/g).join('').length;
+    const myWord = myText.split(" ").filter(s => s.length > 0).length;
+    const mySentence = myText.split(/[.?!...]\s/).length;
+    console.log(myChar, myWord, mySentence );
+
+    document.getElementById('character').innerHTML = myChar;
+    document.getElementById('word').innerHTML = myWord;
+    document.getElementById('sentence').innerHTML = mySentence;
+});
+document.querySelector("#randomcolor").addEventListener('click', function() {
+    document.body.style.backgroundColor = 'rgb(' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ',' + Math.round(Math.random() * 255) + ')';
+
+})
+
+document.querySelector('#reset').addEventListener('click', function() {
+   document.querySelector('input').value = '';
+   document.getElementById('character').innerHTML = 0;
+   document.getElementById('word').innerHTML = 0;
+   document.getElementById('sentence').innerHTML = 0;
+})
